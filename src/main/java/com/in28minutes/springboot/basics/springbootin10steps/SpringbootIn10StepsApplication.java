9ffecc9f762cfc1d6bcc59bd729e199dbf6ee37d
jchpmn <josephchapman@hotmail.com>
2019-03-24 21:01:02 -0500

    update 3-24-19

--- src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -1,13 +1,21 @@
 package com.in28minutes.springboot.basics.springbootin10steps;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+		//ApplicationContext applicationContext =
+				SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+
+//		for (String name : ((ConfigurableApplicationContext) applicationContext).getBeanDefinitionNames()) {
+//			System.out.println(name);
+		}
+
 	}
 
-}
